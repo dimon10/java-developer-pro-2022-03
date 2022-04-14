65993dd1e4fa859621f5bc8ee44c57fb3c2a958d
Dmitry Yanshin <dmitry.ianshin@gmail.com>
2022-04-14 17:52:30 +0300

    homework 02: fixed CustomerReverseOrder (todo 2)

--- hw02-collections/src/main/java/homework/CustomerReverseOrder.java
+++ hw02-collections/src/main/java/homework/CustomerReverseOrder.java
@@ -1,16 +1,17 @@
 package homework;
 
 
+import java.util.LinkedList;
+
 public class CustomerReverseOrder {
 
-    //todo: 2. надо реализовать методы этого класса
-    //надо подобрать подходящую структуру данных, тогда решение будет в "две строчки"
+    private final LinkedList<Customer> linkedList = new LinkedList<>();
 
     public void add(Customer customer) {
-
+        linkedList.add(customer);
     }
 
     public Customer take() {
-        return null; // это "заглушка, чтобы скомилировать"
+         return linkedList.pollLast();
     }
 }
